cb922d1dd12d6995e7366ca9ce263c6f20d2c986
Artur Signell <artur@vaadin.com>
2018-02-09 09:39:40 +0200

    Test for interacting with light DOM only children

--- src/test/java/org/vaadin/artur/shadydomselenium/SeleniumIT.java
+++ src/test/java/org/vaadin/artur/shadydomselenium/SeleniumIT.java
@@ -1,19 +1,34 @@
 package org.vaadin.artur.shadydomselenium;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SeleniumIT {
 
     @Test
-    public void printFoo() {
+    public void accessLightDomElement() throws InterruptedException {
         WebDriver driver = new FirefoxDriver();
         try {
             driver.get("http://localhost:8080/");
-            System.out.println(((JavascriptExecutor) driver)
-                    .executeScript("return 'foo';"));
+
+            // Wait for polyfills to be loaded and grid to be initialized
+            Thread.sleep(1000);
+
+            // Get a reference to the `<vaadin-grid-column>` element only
+            // available in light DOM
+            WebElement grid = driver.findElement(By.id("grid"));
+            WebElement column = (WebElement) ((JavascriptExecutor) driver)
+                    .executeScript("return arguments[0]._getColumns()[0];",
+                            grid);
+
+            // Try to interact with the element
+            System.out.println("Element: " + column);
+            System.out.println(
+                    "Element.outerHTML: " + column.getAttribute("outerHTML"));
         } finally {
             driver.quit();
         }
